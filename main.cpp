e414c8b328b3f6b59e9575134e155a8016beb046
Abhishek Rajak <abhishekrajak100@gmail.com>
2019-04-20 00:56:11 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+/*
+ * Segment Tree using Array
+ **/
+#include <iostream>
+
+using namespace std;
+
+void createSegmentTree(int data[], int tree[], int pos, int low, int high){
+    if(low==high){
+        tree[pos] = data[low];
+        return;
+    }
+    int mid = (low+high)/2;
+    createSegmentTree(data, tree, 2*pos+1, low, mid);
+    createSegmentTree(data, tree, 2*pos+2, mid+1, high);
+    tree[pos] = min(tree[2*pos+1], tree[2*pos+2]);
+}
+
+int minQuery(int tree[], int qlow, int qhigh, int low, int high, int pos){
+    if(qlow<=low && qhigh>=high){
+        return tree[pos];
+    }
+    if(qlow>high || qhigh<low){
+        return INT32_MAX;
+    }
+    int mid = (low+high)/2;
+    return min(minQuery(tree, qlow, qhigh, low, mid, 2*pos+1), minQuery(tree, qlow, qhigh, mid+1, high, 2*pos+2));
+}
+
+int main(){
+
+    /*
+     * if # of elements in array is N
+     * then size of tree must be 4*N-1
+     * */
+
+    return 0;
+}
